11cd9cce6588591f426ae440d1a8a394318ab435
artainmo <53705599+artainmo@users.noreply.github.com>
2023-09-07 00:52:20 +0200

    Delete src directory

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export function drawLine(context, x1, y1, x2, y2)
-{
-    context.beginPath();
-    context.moveTo(x1, y1); // Starting point
-    context.lineTo(x2, y2); // Ending point
-    context.stroke(); // Draw the line
-    context.closePath();
-}
